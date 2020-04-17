afee1b9cec479975c8506db4f72d2f19f1206dbb
jfelix2298 <60903691+jfelix2298@users.noreply.github.com>
2020-04-17 15:08:14 -0700

    Add files via upload

---
+++ phase3/p1.c
@@ -0,0 +1,26 @@
+#include "usloss.h"
+#define  DEBUG 0
+extern int debugflag;
+
+void
+p1_fork(int pid)
+{
+    if (DEBUG && debugflag) {
+        console("p1_fork(): called, pid: %d\n", pid);
+    }
+} /* p1_fork */
+
+void
+p1_switch(int old, int new)
+{
+    if (DEBUG && debugflag)
+        console("p1_switch(): called, old: %d, new: %d\n", old, new);
+} /* p1_switch */
+
+void
+p1_quit(int pid)
+{
+    if (DEBUG && debugflag) {
+        console("p1_quit() called, pid: %d\n", pid);
+    }
+} /* p1_quit */
